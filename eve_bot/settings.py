a8dac97111193237350a0a17c60f2addb76335d1
srinath0307 <srinathmurali2003@gmail.com>
2023-07-18 22:26:38 +0530

    database testing

--- eve_bot/settings.py
+++ eve_bot/settings.py
@@ -76,12 +76,9 @@ WSGI_APPLICATION = "eve_bot.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "CareSupportAI",
-        "USER" : "postgres",
-        "PASSWORD":"12345",
-        "HOST": "localhost"
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
